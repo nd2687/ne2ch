f3bc8c0235fe4a174be7dd12a16ed85296c34fff
Takashi Kawano <kawano.fanta@gmail.com>
2015-04-20 21:22:03 +0900

    To private method check_word_exist & add before_validation remove space code

--- app/models/erotic_word.rb
+++ app/models/erotic_word.rb
@@ -1,19 +1,25 @@
 class EroticWord < ActiveRecord::Base
-  before_save :check_word_exist
+
+  before_validation do
+    self.word = word.gsub(/(\r\n|\r|\n|\f)/,"").strip if word.present?
+  end
 
   before_create do
     self.first_word = first_word.downcase if first_word =~ /^[A-Z]$/
   end
 
+  before_save :check_word_exist
+
+  def self.all_word
+    self.all.map(&:word)
+  end
+
+  private
   def check_word_exist
     if self.class.all_word.include?(word)
       false
     else
       true
     end
-  end
-
-  def self.all_word
-    self.all.map(&:word)
   end
 end
